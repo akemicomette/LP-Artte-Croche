c52745af340543bfffd65b17d6eb25c660ce5c30
akemicomette <akemicomette@gmail.com>
2024-06-21 20:43:33 -0300

    feat: add script.js, styles.css and assets

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const carouselImages = document.querySelector('.carousel-images');
+const slideCount = document.querySelectorAll('.img-intro').length;
+let currentIndex = 0;
+
+document.querySelector('.next-btn').addEventListener('click', () => {
+    if (currentIndex < slideCount - 3) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateCarousel();
+});
+
+document.querySelector('.prev-btn').addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = slideCount - 1;
+    }
+    updateCarousel();
+});
+
+function updateCarousel() {
+    carouselImages.style.left = `${-currentIndex * 30}%`;
+}
